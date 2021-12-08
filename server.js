804fd851d70272638e2ecb530a8f1371bc793d9e
menna131 <65732433+menna131@users.noreply.github.com>
2021-12-09 00:00:38 +0200

    c2

--- server.js
+++ server.js
@@ -2,18 +2,41 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8080;
+app.listen(port, ()=>{
+    console.log(`server is running on port: ${port}`);
+});
+
+
+// post request
+app.post('/postDta', function(req, res){
+    console.log(req.body);
+    projectData = {
+        temp: req.body.temp,
+        userFeeling: req.body.userFeeling,
+        date: req.body.date
+    }
+});
+
+// get request
+app.get('/getData', function(req, res){
+    console.log(res.body);
+    res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,41 @@
 /* Global Variables */
+var projectData = {};
+var basicURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+var apiKey = "74d7ba758febe40bfa66d14fb03d4706";
+var zipcodeValue = document.getElementById('zip');
+var userFeeling = document.getElementById('feelings');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', fetchData);
+
+function fetchData(e){
+    if(zipcodeValue.value === ""){
+        alert("enter zipcode");
+    }else{
+        getData(basicURL, apiKey, zipcodeValue, userFeeling)
+        .then(function(d){ 
+            // update ui
+            updateUI(d);
+        })
+    }
+}
+
+const getData = async function(basicURL, apiKey, zipcodeValue, userFeeling){
+    const res = await fetch(basicURL+zipcodeValue.value+",us&appid="+apiKey)
+    try{
+        const data = await res.json();
+        data.userFeeling = userFeeling.value;
+        return data;
+    }catch(e){
+        console.log("error:", e);
+    }
+}
+
+const updateUI = async function(data){
+    document.getElementById('date').innerHTML = newDate;
+    document.getElementById('temp').innerHTML = data.main.temp;
+    document.getElementById('content').innerHTML = data.userFeeling;
+}
